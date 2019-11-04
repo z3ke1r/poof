021cf836833350c0b9e18e8b5db6311fe45e82f2
Chris <8351123+z3ke1r@users.noreply.github.com>
2019-11-04 14:01:34 -0700

    url again

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataService {
 
-  private REST_API_SERVER = "http://34.67.193.57:4200/farts";
+  private REST_API_SERVER = "http://34.67.193.57:8000/farts";
 
   constructor(private httpClient: HttpClient) { }
   
