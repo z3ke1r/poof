0358946a118d084d20ed8317d4eaa9f1e9f91a91
Chris <8351123+z3ke1r@users.noreply.github.com>
2019-11-04 13:50:31 -0700

    created the http client get request method

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  private REST_API_SERVER = "http://34.67.193.57:4200";
+
+  constructor(private httpClient: HttpClient) { }
+  
+  public sendGetRequest(){
+    return this.httpClient.get(this.REST_API_SERVER);
 }
