288dc2ab4a4b4fa71a8737fdf9a561593ea6c9ca
Chris <8351123+z3ke1r@users.noreply.github.com>
2019-11-04 14:26:36 -0700

    bracket error

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -11,5 +11,5 @@ export class DataService {
   constructor(private httpClient: HttpClient) { }
   
   public sendGetRequest(){
-    return this.httpClient.get(this.REST_API_SERVER);
+    return this.httpClient.get(this.REST_API_SERVER); }
 }
