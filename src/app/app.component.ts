9cdbfff495b5b3b88fb6ab0cca2c433766f9eaee
Chris <8351123+z3ke1r@users.noreply.github.com>
2019-11-04 14:00:31 -0700

    Added html service component to the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'poof';
+
+  farts = [];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+
+    this.dataService.sendGetRequest().subscribe((data: any[])=>{
+      console.log(data);
+      this.farts = data;
+    })  
+  }
 }
