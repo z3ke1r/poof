2a4cf0ab3bbeafb8e7ea626c6fb081766ed65ebe
Chris <8351123+z3ke1r@users.noreply.github.com>
2019-11-05 09:18:23 -0700

    updated service injection variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from './data.service';
-import {Observable} from "rxjs/Observable";
 import {HttpClient} from "@angular/common/http";
-import * as _ from 'lodash';
 
 interface Fart {
     type: string;
@@ -23,15 +21,14 @@ export class AppComponent implements OnInit {
   
   title = 'poof';
 
-  farts = [];
+  farts;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-
-    this.dataService.sendGetRequest().subscribe((data: any[])=>{
+    this.dataService.getFarts().subscribe((data)=>{
       console.log(data);
-      this.farts = data;
+      this.farts = data['farts'];
     })  
   }
 }
