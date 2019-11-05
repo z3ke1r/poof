a92bf81e0ccbc76c944811e379f4f5ab164e2b4f
Chris <8351123+z3ke1r@users.noreply.github.com>
2019-11-05 10:01:51 -0700

    remove service module stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from './data.service';
-import {HttpClient} from "@angular/common/http";
+import { Component } from '@angular/core';
 
-interface Fart {
-    type: string;
-    smell_scale:number;
-    noise_scale:number;
-    score:number;
-    date_farted:Date; 
-    city:string;
-    user:string;
-}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   
-  title = 'poof';
-
-  farts;
-
-  constructor(private dataService: DataService) { }
-
-  ngOnInit() {
-    this.dataService.getFarts().subscribe((data)=>{
-      console.log(data);
-      this.farts = data['farts'];
-    })  
-  }
+  title = 'poof';  
 }
