698227bdc81c16cba4a64c10ef067cac4044b80e
Chris <8351123+z3ke1r@users.noreply.github.com>
2019-11-05 10:06:29 -0700

    adding observable to component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  
+  farts;
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.getFarts().subscribe((data)=>{
+      console.log(data);
+      this.farts = data['farts'];
+    });
   }
 
 }
