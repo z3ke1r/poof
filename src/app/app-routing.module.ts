02740c9b83628632ecd75fac20c14a71602a347f
Chris <8351123+z3ke1r@users.noreply.github.com>
2019-11-19 15:16:53 -0700

    Adding login and register components to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
-  {path:'home', component: HomeComponent}
+  { path:'home', component: HomeComponent },
+  { path: 'login', component: LogInComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
