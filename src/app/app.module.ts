53c6b8407d8d0f3b9b539bb666343c36bfd7c5a3
Chris <8351123+z3ke1r@users.noreply.github.com>
2019-11-21 10:21:04 -0700

    Added missing mat card module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
    MatButtonModule,
+   MatCardModule,
    MatToolbarModule,
    MatIconModule,
    MatBadgeModule,
@@ -41,6 +42,7 @@ import {
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    MatCardModule,
     MatButtonModule,
     MatToolbarModule,
     MatIconModule,
