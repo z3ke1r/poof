c7aee14b591f0fa20201795cb203c0d9f8e3ed04
Chris <8351123+z3ke1r@users.noreply.github.com>
2019-11-21 15:40:23 -0700

    Added profile and feed to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { RegisterComponent } from './register/register.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { FeedComponent } from './feed/feed.component';
 
 
 const routes: Routes = [
   { path:'home', component: HomeComponent },
   { path: 'login', component: LogInComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: RegisterComponent },
+  { path: 'feed', component: RegisterComponent }
 ];
 
 @NgModule({
