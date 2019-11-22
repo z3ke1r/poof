14d995ec1505c41a21ea5b0105956c491a1e66aa
Chris <8351123+z3ke1r@users.noreply.github.com>
2019-11-22 14:39:26 -0700

    Added data service to test error fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
-  title = 'poof';  
+  farts: any;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.dataService.getFarts().subscribe((data)=>{
+      console.log(data);
+      this.farts = data;
+    });
+  }
 }
