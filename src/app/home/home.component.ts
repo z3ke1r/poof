b3618865b35c2cd78ad3bb2c00d5e191bc4c2f3c
Chris <8351123+z3ke1r@users.noreply.github.com>
2019-11-22 14:46:49 -0700

    Removed data service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +7,10 @@ import { DataService } from '../data.service';
 })
 export class HomeComponent implements OnInit {
   
-  farts: any;
 
-  constructor(private dataService: DataService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.dataService.getFarts().subscribe((data)=>{
-      console.log(data);
-      this.farts = data;
-    });
   }
 
 }
