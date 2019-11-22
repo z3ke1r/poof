e450df84fb0ade6b74483cb7d59d735558193196
Chris <8351123+z3ke1r@users.noreply.github.com>
2019-11-22 14:35:44 -0700

    Removed user for now

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataService {
   
-  FART_ENDPOINT = "http://34.66.210.130:8000/farts/1";
+  FART_ENDPOINT = "http://34.66.210.130:8000/farts/2/";
   USER_ENDPOINT = "http://34.66.210.130:8000/users/";
   
 
@@ -15,9 +15,5 @@ export class DataService {
   public getFarts(){
     console.log(this.httpClient.get(this.FART_ENDPOINT));
     return this.httpClient.get(this.FART_ENDPOINT); }
-  
-  public getUsers(){
-    console.log(this.httpClient.get(this.USER_ENDPOINT));
-    return this.httpClient.get(this.USER_ENDPOINT); }
   
 }
