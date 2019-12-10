fff88642cf76668a75ffeac862d87f36fa5f2bac
z3ke1r <crobert8@mail.usf.edu>
2019-12-10 10:55:21 -0700

    removed ssl service endpoint

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -5,15 +5,15 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class DataService {
-  
-  FART_ENDPOINT = "https://104.154.175.134:8443/farts/2/";
-  USER_ENDPOINT = "http://104.154.175.134:8443/users/";
-  
+
+  FART_ENDPOINT = "http://34.67.17.222:8000/farts/2/";
+  USER_ENDPOINT = "http://34.67.17.222:8000/users/";
+
 
   constructor(private httpClient: HttpClient) { }
-  
+
   public getFarts(){
     console.log(this.httpClient.get(this.FART_ENDPOINT));
     return this.httpClient.get(this.FART_ENDPOINT); }
-  
+
 }
