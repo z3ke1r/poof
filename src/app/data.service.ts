85e4e227ba941afa3fd257f13ad9e16ed0c62e4c
Chris <8351123+z3ke1r@users.noreply.github.com>
2019-12-23 12:24:29 -0700

    Added test query service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,12 +8,16 @@ export class DataService {
 
   FART_ENDPOINT = 'http://34.69.4.121:8000/farts/';
   USER_ENDPOINT = 'http://34.69.4.121:8000/users/';
-
+  FART_QUERY = 'http://34.69.4.121:8000/farts?'
 
   constructor(private httpClient: HttpClient) { }
 
   public getFarts() {
     console.log(this.httpClient.get(this.FART_ENDPOINT));
     return this.httpClient.get(this.FART_ENDPOINT); }
+  
+  public getUserFarts(username) {
+    console.log(this.httpClient.get(this.FART_QUERY));
+    return this.httpClient.get(this.FART_QUERY); }
 
 }
