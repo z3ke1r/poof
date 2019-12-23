4178dda094ca24662d9e21cc84219de289334635
Chris <8351123+z3ke1r@users.noreply.github.com>
2019-12-23 12:32:03 -0700

    Added test query for user farts

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  farts: any = [];
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.getUserFarts('admin').subscribe(data => {
+      console.log(data);
+      this.farts = data;
+    });
   }
 
 }
